991ca341ec12b6374b202dc102dadc6f91bc38fb
kellyy8 <kellyyu2886@gmail.com>
2024-11-17 16:10:08 -0800

    Updated links to style and create a navbar for feature pages.

--- VitalMotion/app/index.jsx
+++ VitalMotion/app/index.jsx
@@ -1,30 +1,126 @@
+import { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { Link } from 'expo-router'; 
+import { Link } from 'expo-router';
+import theme from './design_system.js';
 
 export default function Index() {
+  const [isHoveredWorkout, setIsHoveredWorkout] = useState(false);
+  const [isHoveredHistory, setIsHoveredHistory] = useState(false);
+  const [isHoveredNotes, setIsHoveredNotes] = useState(false);
+  const [isHoveredLogin, setIsHoveredLogin] = useState(false);
+  
+  const handleMouseEnter = (page) => {
+    switch (page){
+      case 'Workout':
+        setIsHoveredWorkout(true)
+        break;
+      case 'History':
+        setIsHoveredHistory(true)
+        break;
+      case 'Notes':
+        setIsHoveredNotes(true)
+        break;
+      case 'Login':
+        setIsHoveredLogin(true)
+        break;
+      default:
+        break;
+    }
+  }
+  const handleMouseLeave = (page) => {
+    switch (page){
+      case 'Workout':
+        setIsHoveredWorkout(false)
+        break;
+      case 'History':
+        setIsHoveredHistory(false)
+        break;
+      case 'Notes':
+        setIsHoveredNotes(false)
+        break;
+      case 'Login':
+        setIsHoveredLogin(false)
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/login" style={styles.button}>
-        Go to login screen
-      </Link>
+    <View style={styles.outerWrapper}>
+      
+      <View style={styles.navbar}>
+        <Link
+          href="/workout"
+          style={[styles.pageLink, isHoveredWorkout && styles.pageLinkHovered]}
+          onMouseEnter={()=>{handleMouseEnter('Workout')}}
+          onMouseLeave={()=>{handleMouseLeave('Workout')}}
+        >
+          Workout
+        </Link>
+
+        <Link
+          href="/history"
+          style={[styles.pageLink, isHoveredHistory && styles.pageLinkHovered]}
+          onMouseEnter={()=>{handleMouseEnter('History')}}
+          onMouseLeave={()=>{handleMouseLeave('History')}}
+        >
+          History
+        </Link>
+
+        <Link
+          href="/notes"
+          style={[styles.pageLink, isHoveredNotes && styles.pageLinkHovered]}
+          onMouseEnter={()=>{handleMouseEnter('Notes')}}
+          onMouseLeave={()=>{handleMouseLeave('Notes')}}
+        >
+          Notes 
+        </Link>
+
+        {/** TODO: Should this change based on registering / logging in/out? */}
+        <Link
+          href="/login"
+          style={[styles.pageLink, isHoveredLogin && styles.pageLinkHovered]}
+          onMouseEnter={()=>{handleMouseEnter('Login')}}
+          onMouseLeave={()=>{handleMouseLeave('Login')}}
+        >
+          Login
+        </Link>
+      </View>
+
+      <Text style={styles.innerWrapper}>Home screen (under construction :D) </Text>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  outerWrapper: {
     flex: 1,
-    backgroundColor: '#25292e',
+  },
+  navbar: {                      // TODO: Make navbar sticky.
+    flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'flex-end',
+    gap: 50,
+    padding: 50,
+
+    backgroundColor: theme.colors.aqua,
+  },
+  pageLink: {
+    color: theme.links.default,
+    fontSize: theme.fontSizes.regular,
+    fontWeight: theme.fontWeights.bold,
+  },
+  pageLinkHovered:{
+    color: theme.links.hover,
+  },
+  // TODO: Placeholder styles. Update when actual content is added.
+  innerWrapper: {
+    flex: 1,
+    textAlign: 'center',
     justifyContent: 'center',
-  },
-  text: {
-    color: '#fff',
-  },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff',
+    alignContent: 'center',
+    backgroundColor: theme.colors.grey,
   },
 });
---
+++ VitalMotion/app/workout.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Workout = () => {
+    return (
+        <View>
+            <Text>Workout</Text>
+        </View>
+    );
+};
+
+export default Workout;
