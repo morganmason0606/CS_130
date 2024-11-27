6fbad5221084cf1cb738ca82be7f5b83ff626284
kellyy8 <kellyyu2886@gmail.com>
2024-11-26 22:39:39 -0800

    Updated code format. Associated error messages with corresponding inputs. Updated error handling. In progress, disabled button.

--- VitalMotion/app/login.jsx
+++ VitalMotion/app/login.jsx
@@ -10,17 +10,25 @@ import { useAuth } from './auth_context';
 const LoginScreen = () => {
 	const [firstName, setFirstName] = useState('');
 	const [lastName, setLastName] = useState('');
-        const [email, setEmail] = useState('');
-        const [password, setPassword] = useState('');
-        const [error, setError] = useState('');
-        const [isSignUp, setIsSignUp] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [error, setError] = useState('');						// General error message
+	const [emailError, setEmailError] = useState('');			// Email-specific error message
+	const [passwordError, setPasswordError] = useState('');		// Password-specific error message
+
+    const [isSignUp, setIsSignUp] = useState(false);
   	const [isHovered, setIsHovered] = useState(false);
+	// const [isDisabled, setIsDisabled] = useState(false);
+	
 	const [screenWidth, setScreenWidth] = useState(Dimensions.get('window').width);
+
 	const router = useRouter();
 	const needsToLogin = useRef(false);
 	const [isLoading, setIsLoading] = useState(false);
+
 	// Get the login function from the AuthContext
-        const { login, uid } = useAuth(); 
+    const { login, uid } = useAuth(); 
 
 	// Update width on screen resize
 	useEffect(() => {
@@ -34,6 +42,32 @@ const LoginScreen = () => {
 			Dimensions.removeEventListener('change', onChange);
 		};
 	}, []);
+	
+	// Disable the submit button if any required fields are empty.
+	// useEffect(() => {
+	// 	if (isSignUp){
+	// 		if (!firstName | !lastName | !email | !password) {
+	// 			setIsDisabled(true);
+	// 		}
+	// 		else {
+	// 			setIsDisabled(false);
+	// 		}
+	// 	}
+	// 	else {
+	// 		if (!email | !password) {
+	// 			setIsDisabled(true);
+	// 		}
+	// 		else {
+	// 			setIsDisabled(false);
+	// 		}
+	// 	}
+	// }, [email, password, firstName, lastName]);
+
+	useEffect(() => {
+		setError('');
+		setEmailError('');
+		setPasswordError('');
+	}, [email, password, firstName, lastName]);
 
 	useEffect(() => {
 		if ((uid !== null)  && !needsToLogin && !isSignUp && !isLoading) {
@@ -43,9 +77,11 @@ const LoginScreen = () => {
 	}, [uid, needsToLogin, isSignUp]);
 
     const handleLogin = async () => {
-	    setError('');
-        if (!password) {
-            setError('Password cannot be empty.');
+		setError('');
+		setEmailError('');
+		setPasswordError('');
+		if (!email | !password) {
+            setError('Please fill in all fields.');
             return;
         }
 	    console.log('Attempting login with:', email, password);
@@ -57,7 +93,7 @@ const LoginScreen = () => {
 		const response = await fetch('http://localhost:5001/verify-token', {
 		    method: 'POST',
 		    headers: {
-			'Content-Type': 'application/json',
+				'Content-Type': 'application/json',
 		    },
 		    body: JSON.stringify({ token: idToken }),
 		});
@@ -66,7 +102,7 @@ const LoginScreen = () => {
 
 		if (response.ok) {
 		    console.log('Login Successful!', `Welcome, User ID: ${data.uid}`);
-                    alert('Login Successful!', `Welcome, User ID: ${data.uid}`);
+            alert('Login Successful!', `Welcome, User ID: ${data.uid}`);
 		    login(data.uid);
 		    needsToLogin.current = false;
 		    router.push('/');
@@ -74,20 +110,22 @@ const LoginScreen = () => {
 		    console.log('Login Failed', data.error);
 		}
 	    } catch (error) {
-		console.error('Error during login:', error);
-		handleFireBaseError(error);
+			console.error('Error during login:', error);
+			handleFireBaseError(error);
 	    }
     };
 
     const handleSignUp = async () => {
         setError('');
-		if (!firstName | !lastName | !email | !password) {
+		setEmailError('');
+		setPasswordError('');
+		if (!firstName && !lastName && !email && !password) {
             setError('Please fill in all fields.');
             return;
-        }
+		}
         try {
-	    needsToLogin.current = true;
-	    setIsLoading(true);
+	    	needsToLogin.current = true;
+	    	setIsLoading(true);
             const userCredential = await createUserWithEmailAndPassword(auth, email, password);
             console.log('User signed up:', userCredential.user);
 			const idToken = await userCredential.user.getIdToken();
@@ -97,7 +135,7 @@ const LoginScreen = () => {
 			const response = await fetch('http://localhost:5001/verify-token', {
 				method: 'POST',
 				headers: {
-				'Content-Type': 'application/json',
+					'Content-Type': 'application/json',
 				},
 				body: JSON.stringify({ token: idToken }),
 			});
@@ -105,9 +143,9 @@ const LoginScreen = () => {
 
 			if (response.ok) {
 				const setup_response = await fetch('http://localhost:5001/setup-user', {
-					    method: 'POST',
-			    headers: {
-					'Content-Type': 'application/json',
+					method: 'POST',
+			    	headers: {
+						'Content-Type': 'application/json',
 				    },
 				    body: JSON.stringify({ uid: data.uid, firstName:firstName, lastName:lastName}),
 				});
@@ -119,36 +157,36 @@ const LoginScreen = () => {
 			setIsLoading(false);
         } catch (error) {
             handleFireBaseError(error);
-	    setIsLoading(false);
+	    	setIsLoading(false);
         }
     };
 
     const handleFireBaseError = (error) => {
-	// Map Firebase authentication errors to user-friendly messages
+		// Map Firebase authentication errors to user-friendly messages
 	    // Credit for this code to google firebase documentation and my past self in CS35L
+        setError('');
+		setEmailError('');
+		setPasswordError('');
 	    switch (error.code) {
-	      case "auth/invalid-credential":
-			setError("Invalid username or password");
-			break;
-	      case "auth/invalid-email":
-			setError("Invalid email address. Please provide a valid email.");
-			break;
-	      case "auth/user-not-found":
-			setError("User not found. Please sign up before logging in.");
-			break;
-	      case "auth/email-already-in-use":
-			setError("This email is already in use. Try logging in.");
-			break;
-	      case "auth/invalid-email":
-			setError("Invalid email address. Please provide a valid email.");
-			break;
-	      case "auth/weak-password":
-			setError("Password is too weak. Please use a stronger password.");
-			break;	
-	      // Add more cases for other Firebase error codes as needed
-	      default:
-			setError("An error occurred. Please try again later.");
-			break;
+			case "auth/invalid-credential":
+				setError("Invalid email address or password. Please try again.");
+				break;
+			case "auth/invalid-email":
+				setEmailError("Invalid email address. Please provide a valid email.");
+				break;
+			case "auth/user-not-found":
+				setError("User not found. Please sign up before logging in.");
+				break;
+			case "auth/email-already-in-use":
+				setEmailError("This email is already in use. Try logging in.");
+				break;
+			case "auth/weak-password":
+				setPasswordError("Password is too weak. Please use a stronger password.");
+				break;	
+			// Add more cases for other Firebase error codes as needed
+			default:
+				setError("An error occurred. Please try again later.");
+				break;
 	    }
 	  };
 	
@@ -198,7 +236,7 @@ const LoginScreen = () => {
 				value={email}
 				onChangeText={setEmail}
 			/>
-			{(error !== '') && <Text style={theme.error}>{error}</Text>}
+			{(emailError !== '') && <Text style={theme.error}>{emailError}</Text>}
 
 			<Text style={styles.label}>
 				Password
@@ -211,11 +249,26 @@ const LoginScreen = () => {
 				value={password}
 				onChangeText={setPassword}
 			/>
+			{(passwordError !== '') && <Text style={theme.error}>{passwordError}</Text>}
+
+			{(error !== '') && <Text style={[theme.error, {textAlign: 'center', marginTop: '1rem'}]}>{error}</Text>}
 
             {isSignUp ? (
-				<Text style={styles.submitButton} onPress={handleSignUp}>{isLoading ? 'Loading...' : 'Register'}</Text>
+				<Text
+					style={styles.submitButton} // isDisabled && styles.disabledButton
+					// pointerEvents={isDisabled ? 'none' : 'auto'}
+					onPress={handleSignUp}
+				>
+					{isLoading ? 'Loading...' : 'Register'}
+				</Text>
             ) : (
-                <Text style={styles.submitButton} onPress={handleLogin}>Login</Text>
+                <Text
+					style={styles.submitButton} // isDisabled && styles.disabledButton
+					// pointerEvents={isDisabled ? 'none' : 'auto'}
+					onPress={handleLogin}
+				>
+					Login
+				</Text>
             )}
 
 			<Text
