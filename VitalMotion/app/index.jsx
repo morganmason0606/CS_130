ca64d7e5f2436d838042a8666b4d8fde055c701a
kellyy8 <kellyyu2886@gmail.com>
2024-11-28 16:27:09 -0800

    Reroute use to Workout page when they log in.

--- VitalMotion/app/index.jsx
+++ VitalMotion/app/index.jsx
@@ -3,19 +3,20 @@ import Navbar from './navbar.js';
 import styles from './index_styles.js';
 import { useAuth } from './auth_context.js';
 import { useEffect } from 'react';
-import { useRouter, rootNavigationState } from 'expo-router';
+import { useRouter } from 'expo-router';
 
 export default function Index() {
     const { uid } = useAuth();
     const router = useRouter();
 
     useEffect(() => {
-       if (uid === null) {
-          setTimeout(() => {router.push('/login');}, 800);
-       }
-       else {
-	  console.log('User ID:', uid);
-       }
+      if (uid === null) {
+        setTimeout(() => {router.push('/login');}, 800);
+      }
+      else {
+        setTimeout(() => {router.push('/workout');}, 800);
+        console.log('User ID:', uid);
+      }
     }, [uid]); // Runs whenever uid changes
 
   return (
